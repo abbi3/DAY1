045d94f0f8fc0e0a3991c393b59a2fd091a6b2e1
Abby <abbyjamali123@gmail.com>
2018-12-10 15:20:48 +0530

    .

---
+++ assignment11.py
@@ -0,0 +1,8 @@
+#identify set of best practices,be able to implement the techniques during coding.
+itemno = 1005
+unit_price = 250
+quantity = 2
+amount = quantity*unit_price
+print("item_no:",itemno)
+print("Bill_amount:",amount)
+
